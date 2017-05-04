231e4fc6a944cbb4e54e2e1c290fa1dfa1989d03
Neil H Watson <neil@watson-wilson.ca>
2017-05-04 14:19:10 -0400

    Update tf-init.sh

--- azure/vm-provisioning/tf-init.sh
+++ azure/vm-provisioning/tf-init.sh
@@ -3,6 +3,6 @@
 terraform init \
    -backend-config="access_key=${ARM_ACCESS_KEY}" \
    -backend-config="container_name=terraform-state" \
-   -backend-config="storage_account_name=npterraform" \
+   -backend-config="storage_account_name=my-terraform-state" \
    -backend-config="key=nwatson-test01.terraform.tfstate"
 
