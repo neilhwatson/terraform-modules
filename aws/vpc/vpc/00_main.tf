a31b64d622cf0ad63ef464b4501efb274db6b626
Neil H watson <neil@watson-wilson.ca>
2017-11-05 10:03:28 -0500

    fix tags

--- aws/vpc/vpc/00_main.tf
+++ aws/vpc/vpc/00_main.tf
@@ -4,7 +4,7 @@ variable "cidr_block" {}
 resource "aws_vpc" "vpc01" {
    cidr_block                       = "${var.cidr_block}"
    assign_generated_ipv6_cidr_block = "true"
-   tags                             = "${var.tag}"
+   tags                             = { "${var.tag}" }
 }
 
 output "vpc01_id" {
