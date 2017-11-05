1700109bc93257be8a12dfbacdebab323caec977
Neil H watson <neil@watson-wilson.ca>
2017-11-05 16:38:20 -0500

    VPC subnet

--- aws/vpc/subnet/00_main.tf
+++ aws/vpc/subnet/00_main.tf
@@ -3,11 +3,15 @@ variable "cidr_block" {}
 variable "vpc_id" {}
 
 resource "aws_subnet" "subnet01" {
-   vpc_id                           = "${var.vpc_id}"
-   cidr_block                       = "${var.cidr_block}"
-   tags                             = "${var.tag}"
+   vpc_id          = "${var.vpc_id}"
+   cidr_block      = "${var.cidr_block}"
+   ipv6_cidr_block = "${var.ipv6_cidr_block}"
+   tags            = "${var.tag}"
 }
 
 output "cidr_block" {
    value = "${aws_subnet.subnet01.cidr_block}"
+}
+output "ipv6_cidr_block" {
+   value = "${aws_subnet.subnet01.ipv6_cidr_block}"
 }
