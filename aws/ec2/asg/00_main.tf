f542e82d6a455479ef6522a4c5b1dd41551aa4f6
Neil H watson <neil@watson-wilson.ca>
2017-11-05 18:31:25 -0500

    ASG testing

--- aws/ec2/asg/00_main.tf
+++ aws/ec2/asg/00_main.tf
@@ -1,21 +1,17 @@
 #
 # Variables
 #
-variable "ami_id" {}
-variable "instance_type" { default = "t2.micro" }
-variable "vpc_zone_ids" { type = "list" }
-variable "security_groups" { type = "list" }
-variable "min_size" { default = 1 }
-variable "max_size" { default = 1 }
-variable "desired_capacity" { default = 1 }
-variable "user_data_file" {}
-variable "tag" { type = "map" }
-
-
-#variable "vpc" {}
-#variable "subnet" {}
-variable "ssh_key" {}
-#variable "iam_instance_profile" {}
+variable "ami_id"                { }
+variable "instance_type"         { default = "t2.micro" }
+variable "vpc_zone_ids"          { type = "list" }
+variable "security_groups"       { type = "list" }
+variable "min_size"              { default = 1 }
+variable "max_size"              { default = 1 }
+variable "desired_capacity"      { default = 1 }
+variable "user_data_file"        { }
+variable "tag"                   { type = "map" }
+variable "ssh_key"               { }
+#variable "iam_instance_profile" { }
 
 resource "aws_launch_configuration" "launch_conf01" {
     image_id        = "${var.ami_id}"
@@ -29,11 +25,10 @@ resource "aws_launch_configuration" "launch_conf01" {
 
 resource "aws_autoscaling_group" "asg01" {
     launch_configuration = "${aws_launch_configuration.launch_conf01.name}"
-
-    vpc_zone_identifier = [ "${var.vpc_zone_ids}" ]
-    min_size = "${var.min_size}"
-    max_size = "${var.max_size}"
-    desired_capacity = "${var.desired_capacity}"
-#    tags            = "${var.tag}"
+    vpc_zone_identifier  = [ "${var.vpc_zone_ids}" ]
+    min_size             = "${var.min_size}"
+    max_size             = "${var.max_size}"
+    desired_capacity     = "${var.desired_capacity}"
+#    tags                = "${var.tag}"
 
 }
