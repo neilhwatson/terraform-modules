deba56cee9838de03bf4da9ac7b241ed56aab057
Neil H watson <neil@watson-wilson.ca>
2017-11-05 12:20:55 -0500

    VPC SG testing

--- aws/vpc/vpc/00_main.tf
+++ aws/vpc/vpc/00_main.tf
@@ -1,14 +1,15 @@
 variable "tag" { type = "map" }
 variable "cidr_block" {}
-variable "default_sg" {}
 
 resource "aws_vpc" "vpc01" {
    cidr_block                       = "${var.cidr_block}"
    assign_generated_ipv6_cidr_block = "true"
    tags                             = "${var.tag}"
-   default_security_group_id        = "${var.default_sg}"
 }
 
 output "vpc01_id" {
    value = "${aws_vpc.vpc01.id}"
+}
+output "ipv6_cidr_block" {
+   value = "${aws_vpc.vpc01.ipv6_cidr_block}"
 }
