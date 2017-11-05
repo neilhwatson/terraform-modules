217c02b5fa2254d47ccd68d078f09c3b249e5c0e
Neil H watson <neil@watson-wilson.ca>
2017-11-05 11:29:34 -0500

    security group testing

--- aws/vpc/sg/ssh-in/main.tf
+++ aws/vpc/sg/ssh-in/main.tf
@@ -2,25 +2,25 @@ variable "security_group_name" { default = "ssh-in" }
 variable "tag"                 { type = "map" }
 variable "vpc_id"              {}
 variable "source_cidr_blocks" {
-   type = "list"
+   type    = "list"
    default = [ "0.0.0.0/0" ]
 }
 variable "ipv6_source_cidr_blocks" {
-   type = "list"
+   type    = "list"
    default = [ "::/0" ]
 }
 
 resource "aws_security_group" "sg01" {
-    name = "${var.security_group_name}"
+    name        = "${var.security_group_name}"
     description = "SSH in ${var.security_group_name}"
-    vpc_id = "${var.vpc_id}"
+    vpc_id      = "${var.vpc_id}"
 
     // allow traffic for TCP ssh
     ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["${var.source_cidr_blocks}"]
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["${var.source_cidr_blocks}"]
         ipv6_cidr_blocks = ["${var.ipv6_source_cidr_blocks}"]
     }
 }
