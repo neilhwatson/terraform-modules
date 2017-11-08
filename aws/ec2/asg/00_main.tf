eeb85bb284deeab6b04c4606ac2e24b4cd0b0fa3
Neil H watson <neil@watson-wilson.ca>
2017-11-07 21:27:23 -0500

    ASG add lifecycle

--- aws/ec2/asg/00_main.tf
+++ aws/ec2/asg/00_main.tf
@@ -16,22 +16,23 @@ variable "associate_public_ip" { default = "false" }
 #variable "iam_instance_profile" { }
 
 resource "aws_launch_configuration" "launch_conf01" {
-    image_id        = "${var.ami_id}"
-    instance_type   = "${var.instance_type}"
-    user_data       = "${file("${var.user_data_file}")}"
-    key_name        = "${var.ssh_key}"
-    security_groups = [ "${var.security_groups}" ]
-    associate_public_ip_address = "true"
+   image_id        = "${var.ami_id}"
+   instance_type   = "${var.instance_type}"
+   user_data       = "${file("${var.user_data_file}")}"
+   key_name        = "${var.ssh_key}"
+   security_groups = [ "${var.security_groups}" ]
+   associate_public_ip_address = "true"
 #    tags            = "${var.tag}"
 
+   lifecycle { create_before_destroy = true }
 }
 
 resource "aws_autoscaling_group" "asg01" {
-    launch_configuration = "${aws_launch_configuration.launch_conf01.name}"
-    vpc_zone_identifier  = [ "${var.vpc_zone_ids}" ]
-    min_size             = "${var.min_size}"
-    max_size             = "${var.max_size}"
-    desired_capacity     = "${var.desired_capacity}"
+   launch_configuration = "${aws_launch_configuration.launch_conf01.name}"
+   vpc_zone_identifier  = [ "${var.vpc_zone_ids}" ]
+   min_size             = "${var.min_size}"
+   max_size             = "${var.max_size}"
+   desired_capacity     = "${var.desired_capacity}"
 #    tags                = "${var.tag}"
 
 }
