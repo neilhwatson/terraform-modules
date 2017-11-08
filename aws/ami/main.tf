545eeef0ede41589390f72cf2f81ce2c6e8e79c0
Neil H watson <neil@watson-wilson.ca>
2017-11-07 20:09:11 -0500

    AMI testing

--- aws/ami/main.tf
+++ aws/ami/main.tf
@@ -1,23 +1,35 @@
-variable "architecture" { default = "x86_64" }
-variable "size"         { default = "*"          }
-variable "name"         { default = "Amazon Linux AMI*" }
-variable "owner"        { default = "*" }
+variable "architecture"        { default = "x86_64" }
+variable "description"         { default = "*" }
+variable "name"                { default = "Amazon Linux AMI*" }
+variable "owner"               { default = "*" }
+variable "root-device-type"    { default = "*" }
+variable "virtualization-type" { default = "*" }
 
 data "aws_ami" "nat_ami" {
    most_recent = true
-   owners = [ "${var.owner}" ]
 
    filter {
       name   = "architecture"
       values = ["${var.architecture}"]
    }
    filter {
+      name = "description"
+      values = [ "${var.description}" ]
+   }
+   filter {
       name = "name"
       values = [ "${var.name}" ]
    }
+   
+   owners = [ "${var.owner}" ]
+
    filter {
-      name   = "block-device-mapping.volume-size"
-      values = ["${var.size}"]
+      name   = "root_device_type"
+      values = ["${var.root-device-type}"]
+   }
+   filter {
+      name   = "virtualization_type"
+      values = ["${var.virtualization-type}"]
    }
 }
 
