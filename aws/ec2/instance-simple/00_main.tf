edfd5403bdb5c114e51101c01e1fa260ab418927
Neil H watson <neil@watson-wilson.ca>
2017-11-20 16:51:22 -0500

    New simple ec2 instance, add tags map

--- aws/ec2/instance-simple/00_main.tf
+++ aws/ec2/instance-simple/00_main.tf
@@ -8,6 +8,7 @@ variable "instance_type"       { default = "t2.micro" }
 variable "ipv6_count"          { default = 1 }
 variable "profile"             { }
 variable "ssh_key"             { }
+variable "user_data_file"      { }
 # Passing a map of tags does not work in TF. Waiting on improvement.
 #variable "tags"                { type = "map" }
 
