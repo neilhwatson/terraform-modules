82491440977d31e52fbe4649977ba681524b3b16
Neil H watson <neil@watson-wilson.ca>
2017-11-20 16:50:10 -0500

    New simple ec2 instance, add tags map

--- aws/ec2/instance-simple/00_main.tf
+++ aws/ec2/instance-simple/00_main.tf
@@ -8,9 +8,8 @@ variable "instance_type"       { default = "t2.micro" }
 variable "ipv6_count"          { default = 1 }
 variable "profile"             { }
 variable "ssh_key"             { }
-variable "tags"                { type = "map" }
-
-# TODO security group for ssh and cfengine in
+# Passing a map of tags does not work in TF. Waiting on improvement.
+#variable "tags"                { type = "map" }
 
 resource "aws_instance" "instance01" {
 ami                         = "${var.ami}"
@@ -19,7 +18,6 @@ iam_instance_profile        = "${var.profile}"
 instance_type               = "${var.instance_type}"
 ipv6_address_count          = "${var.ipv6_count}"
 key_name                    = "${var.ssh_key}"
-tags                        = "${var.tags}"
 user_data                   = "${file("${var.user_data_file}")}"
 }
 
