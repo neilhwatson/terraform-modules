4c620c0dd885d4f65dc066e0c8acdc284ac83387
Neil H watson <neil@watson-wilson.ca>
2018-08-14 18:14:57 -0400

    Make subnet ipv4 and ipv6 options

--- aws/vpc/subnet/00_main.tf
+++ aws/vpc/subnet/00_main.tf
@@ -3,13 +3,16 @@ variable "cidr_block" {}
 variable "ipv6_cidr_block" {}
 variable "vpc_id" {}
 variable "az" {}
+variable "public_ipv4" { default = false }
+variable "ipv6" { default = true }
 
 resource "aws_subnet" "subnet01" {
    vpc_id                          = "${var.vpc_id}"
    cidr_block                      = "${var.cidr_block}"
    ipv6_cidr_block                 = "${var.ipv6_cidr_block}"
    availability_zone               = "${var.az}"
-   assign_ipv6_address_on_creation = "true"
+   map_public_ip_on_launch         = "${var.public_ipv4}"
+   assign_ipv6_address_on_creation = "${var.ipv6}"
    tags                            = "${var.tag}"
 }
 
