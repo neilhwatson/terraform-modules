0e8e5969c2c3a611a31ce7382f6a698b234db471
Neil H watson <neil@watson-wilson.ca>
2019-11-29 17:52:14 -0500

    subnet to tf v .12

--- aws/vpc/subnet/00_main.tf
+++ aws/vpc/subnet/00_main.tf
@@ -1,4 +1,4 @@
-variable "tag" { type = "map" }
+variable "tag" { type = map }
 variable "cidr_block" {}
 variable "ipv6_cidr_block" {}
 variable "vpc_id" {}
@@ -7,21 +7,21 @@ variable "public_ipv4" { default = false }
 variable "ipv6" { default = true }
 
 resource "aws_subnet" "subnet01" {
-   vpc_id                          = "${var.vpc_id}"
-   cidr_block                      = "${var.cidr_block}"
-   ipv6_cidr_block                 = "${var.ipv6_cidr_block}"
-   availability_zone               = "${var.az}"
-   map_public_ip_on_launch         = "${var.public_ipv4}"
-   assign_ipv6_address_on_creation = "${var.ipv6}"
-   tags                            = "${var.tag}"
+   vpc_id                          = var.vpc_id
+   cidr_block                      = var.cidr_block
+   ipv6_cidr_block                 = var.ipv6_cidr_block
+   availability_zone               = var.az
+   map_public_ip_on_launch         = var.public_ipv4
+   assign_ipv6_address_on_creation = var.ipv6
+   tags                            = var.tag
 }
 
 output "id" {
-   value = "${aws_subnet.subnet01.id}"
+   value = aws_subnet.subnet01.id
 }
 output "cidr_block" {
-   value = "${aws_subnet.subnet01.cidr_block}"
+   value = aws_subnet.subnet01.cidr_block
 }
 output "ipv6_cidr_block" {
-   value = "${aws_subnet.subnet01.ipv6_cidr_block}"
+   value = aws_subnet.subnet01.ipv6_cidr_block
 }
