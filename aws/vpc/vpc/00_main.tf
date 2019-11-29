2a25780ee28dd31da03e0b16de291fff9b3081df
Neil H watson <neil@watson-wilson.ca>
2019-11-29 17:51:32 -0500

    vpc to tf v .12

--- aws/vpc/vpc/00_main.tf
+++ aws/vpc/vpc/00_main.tf
@@ -1,19 +1,19 @@
-variable "tag" { type = "map" }
+variable "tag" { type = map }
 variable "cidr_block" {}
 
 resource "aws_vpc" "vpc01" {
-   cidr_block                       = "${var.cidr_block}"
+   cidr_block                       = var.cidr_block
    assign_generated_ipv6_cidr_block = "true"
    enable_dns_hostnames             = "true"
-   tags                             = "${var.tag}"
+   tags                             = var.tag
 }
 
 output "vpc01_id" {
-   value = "${aws_vpc.vpc01.id}"
+   value = aws_vpc.vpc01.id
 }
 output "ipv6_cidr_block" {
-   value = "${aws_vpc.vpc01.ipv6_cidr_block}"
+   value = aws_vpc.vpc01.ipv6_cidr_block
 }
 output "default_sg_id" {
-   value = "${aws_vpc.vpc01.default_security_group_id}"
+   value = aws_vpc.vpc01.default_security_group_id
 }
