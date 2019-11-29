fecb28a4260752348d70cc9240bfcec23d183e4d
Neil H watson <neil@watson-wilson.ca>
2019-11-29 16:20:55 -0500

    Change to terraform v.12

--- aws/ami/main.tf
+++ aws/ami/main.tf
@@ -9,25 +9,25 @@ data "aws_ami" "ami01" {
 
    filter {
       name   = "architecture"
-      values = ["${var.architecture}"]
+      values = [var.architecture]
    }
    filter {
       name = "description"
-      values = [ "${var.description}" ]
+      values = [ var.description ]
    }
    
-   owners = [ "${var.owner}" ]
+   owners = [ var.owner ]
 
    filter {
       name   = "root-device-type"
-      values = ["${var.root_device_type}"]
+      values = [var.root_device_type]
    }
    filter {
       name   = "virtualization-type"
-      values = ["${var.virtualization_type}"]
+      values = [var.virtualization_type]
    }
 }
 
 output "image_id" {
-   value = "${data.aws_ami.ami01.image_id}"
+   value = data.aws_ami.ami01.image_id
 }
--- aws/ec2/asg/00_main.tf
+++ aws/ec2/asg/00_main.tf
@@ -18,25 +18,25 @@ variable "asg_name"            { default = "default" }
 #variable "iam_instance_profile" { }
 
 resource "aws_launch_configuration" "launch_conf01" {
-   image_id                    = "${var.ami_id}"
-   instance_type               = "${var.instance_type}"
-   user_data                   = "${var.user_data}"
-   key_name                    = "${var.ssh_key}"
-   security_groups             = [ "${var.security_groups}" ]
-   iam_instance_profile        = "${var.instance_profile}"
+   image_id                    = var.ami_id
+   instance_type               = var.instance_type
+   user_data                   = var.user_data
+   key_name                    = var.ssh_key
+   security_groups             = [ var.security_groups ]
+   iam_instance_profile        = var.instance_profile
    associate_public_ip_address = "true"
-#    tags                      = "${var.tag}"
+#    tags                      = var.tag
 
    lifecycle { create_before_destroy = true }
 }
 
 resource "aws_autoscaling_group" "asg01" {
-   name                 = "${var.asg_name}"
-   launch_configuration = "${aws_launch_configuration.launch_conf01.name}"
-   vpc_zone_identifier  = [ "${var.vpc_zone_ids}" ]
-   min_size             = "${var.min_size}"
-   max_size             = "${var.max_size}"
-   desired_capacity     = "${var.desired_capacity}"
-#    tags                = "${var.tag}"
+   name                 = var.asg_name
+   launch_configuration = aws_launch_configuration.launch_conf01.name
+   vpc_zone_identifier  = [ var.vpc_zone_ids ]
+   min_size             = var.min_size
+   max_size             = var.max_size
+   desired_capacity     = var.desired_capacity
+#    tags                = var.tag
 
 }
